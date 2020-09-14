5f1f80be238f2f5d3a1f30602354045d08c274b0
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-09-14 11:07:48 -0300

    :sparkles: Creates history page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdArrowBack } from 'react-icons/md';
+
+import './styles.scss';
+
+const History: React.FC = () => {
+    return (
+        <div id="history-page" className="container">
+            <Link to="/">
+                <div className="back">
+                    <MdArrowBack size={22} />
+                    <span>Voltar</span>
+                </div>
+            </Link>
+            <h2 className="title">Seus últimos cálculos fatorais.</h2>
+
+            {
+                false &&
+                <ul className="history">
+                    {
+                        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].map((item) => (
+                            <li key={String(item)} className="history-item">
+                                <span className="factorial">14</span>
+                                <span className="result">9823823</span>
+                            </li>
+                        ))
+                    }
+                </ul>
+            }
+
+            {
+                true &&
+                <p>
+                    Você ainda não realizou nenhum cálculo.
+                </p>
+            }
+        </div>
+    );
+}
+
+export default History;
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { AiFillHeart } from 'react-icons/ai'
 
 import Home from './Home';
+import History from './History';
 
 import Logo from '../assets/logo.svg';
 import LogoColored from '../assets/logo-colored.svg';
@@ -11,7 +12,9 @@ const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <header>
-                <img src={Logo} alt="Logo" />
+                <Link to="/">
+                    <img src={Logo} alt="Logo" />
+                </Link>
                 <h1 className="title">Faça seus cálculos de fatorial de maneira simples e rápida.</h1>
 
                 <p className="author">
@@ -19,9 +22,12 @@ const Routes: React.FC = () => {
                 </p>
             </header>
             <div className="router">
-                <img src={LogoColored} alt="Logo" />
+                <Link className="logo" to="/">
+                    <img src={LogoColored} alt="Logo" />
+                </Link>
                 <Switch>
-                    <Route path="/" component={Home} />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/history" component={History} />
                 </Switch>
             </div>
         </BrowserRouter>
