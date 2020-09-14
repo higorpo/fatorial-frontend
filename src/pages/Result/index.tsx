c7919cf76c40e99b0c8cd3348a029b0ccb7b6a87
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-09-14 13:58:28 -0300

    :sparkles: Connect with the API

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { MdRefresh } from 'react-icons/md';
 import { Link, useLocation } from 'react-router-dom';
+import api from '../../config/api';
 
 import './styles.scss';
 
@@ -11,6 +12,33 @@ function useQuery() {
 const Result: React.FC = () => {
     const query = useQuery();
     const factorialNumber = Number(query.get("factorial"));
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [errors, setErrors] = useState<string>("");
+    const [factorialResult, setFactorialResult] = useState<string>("");
+
+    useEffect(() => {
+        setLoading(true);
+        setErrors("");
+        setFactorialResult("");
+
+        async function getFactorial() {
+            if (factorialNumber) {
+                const response = await fetch(`${api.baseURL}/factorial?number=${factorialNumber}`)
+                if (response.status === 200) {
+                    const { result } = await response.json();
+                    setFactorialResult(result);
+                    setLoading(false);
+                } else {
+                    const { errors } = await response.json();
+                    setErrors(errors[0].msg);
+                    setLoading(false);
+                }
+            }
+        }
+
+        getFactorial();
+    }, [factorialNumber])
 
     const multipliers = useMemo(() => {
         if (factorialNumber <= 20) {
@@ -33,24 +61,42 @@ const Result: React.FC = () => {
     }
 
     return (
-        <div className="container">
+        <div id="result-page" className="container">
             <h2 className="title">
                 Resultado do cálculo de {factorialNumber} fatorial é de:
             </h2>
 
-            <div className="factorial-result">
-                <p className="multipliers">{multipliers}</p>
-                <h4 className="result">
-                    51391281291
-                </h4>
-            </div>
+            {
+                loading ?
+                    <>
+                        <p>Aguarde, computando...</p>
+                    </>
+                    :
+                    <>
+                        {
+                            errors ?
+                                <>
+                                    <p><strong>ERRO:</strong> {errors}</p>
+                                </>
+                                :
+                                <>
+                                    <div className="factorial-result">
+                                        <p className="multipliers">{multipliers}</p>
+                                        <h4 className="result">
+                                            {factorialResult}
+                                        </h4>
+                                    </div>
 
-            <Link to="/" className="back-button">
-                <div className="back">
-                    <MdRefresh size={22} />
-                    <span>Novo cálculo</span>
-                </div>
-            </Link>
+                                </>
+                        }
+                        <Link to="/" className="back-button">
+                            <div className="back">
+                                <MdRefresh size={22} />
+                                <span>Novo cálculo</span>
+                            </div>
+                        </Link>
+                    </>
+            }
         </div>
     );
 }
