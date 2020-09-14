f26b80801ea11e0e5c0ba7f4df050aad78e77c5b
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-09-14 09:36:05 -0300

    :sparkles: Create home structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { MdChevronRight } from 'react-icons/md'
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
     return (
         <div className="container">
-            <h1>Hello World :)</h1>
+            <hgroup>
+                <h1 className="title">Digite um número para calcular sua fatorial.</h1>
+                <h3 className="explanation">Digite um número entre 1 a 150 para calcular o seu valor fatorial de maneira simples e rápida. Seus cálculos ficarão salvos para que você possa consultá-los no futuro.</h3>
+            </hgroup>
+
+            <form>
+                <input
+                    type="number"
+                    name="fatorial_number"
+                    placeholder="Número"
+                />
+
+                <button type="submit">
+                    <MdChevronRight />
+                </button>
+            </form>
+
+            <nav>
+                <Link to="/history">Histórico de cálculos</Link>
+            </nav>
         </div>
     );
 }
