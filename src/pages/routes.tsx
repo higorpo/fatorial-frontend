7243ce96d12ad65c4aa78e7bfbc12bbb1eb81edd
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-09-14 10:31:54 -0300

    :sparkles: Finalize home page styles

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AiFillHeart } from 'react-icons/ai'
+
 import Home from './Home';
+
+import Logo from '../assets/logo.svg';
+import LogoColored from '../assets/logo-colored.svg';
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
+            <header>
+                <img src={Logo} alt="Logo" />
+                <h1 className="title">Faça seus cálculos de fatorial de maneira simples e rápida.</h1>
+
+                <p className="author">
+                    Feito com <AiFillHeart /> por Higor Oliveira
+                </p>
+            </header>
+            <div className="router">
+                <img src={LogoColored} alt="Logo" />
+                <Switch>
+                    <Route path="/" component={Home} />
+                </Switch>
+            </div>
         </BrowserRouter>
     );
 }
